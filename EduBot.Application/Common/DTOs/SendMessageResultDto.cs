37034ba9ad03f4a01418b346cf889ce1b2c4ce15
Matheus Amaral <m.amaral8998@gmail.com>
2024-04-27 21:06:20 -0300

    EDU-019 - retornando os buttons das mensagens do rasa para o front

--- EduBot.Application/Common/DTOs/SendMessageResultDto.cs
+++ EduBot.Application/Common/DTOs/SendMessageResultDto.cs
@@ -6,5 +6,14 @@ namespace EduBot.Application.Common.DTOs {
         public string RecipientId { get; set; } = string.Empty;
         [JsonPropertyName("text")]
         public string Text { get; set; } = string.Empty;
+        [JsonPropertyName("buttons")]
+        public IEnumerable<ButtonDto> Buttons { get; set; } = new List<ButtonDto>();
+    }
+    public class ButtonDto {
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("payload")]
+        public string Payload { get; set; } = string.Empty;
     }
 }
